c3a4ddb7f4cdbdec237315ac6e9114b08ec74cef
Andrew Haller <stairwaytowonderland@gmail.com>
2023-05-12 17:55:49 -0500

    Adding additional tj-actions.

---
+++ .github/actions/glob-action/src/cleanup.ts
@@ -0,0 +1,19 @@
+import * as core from '@actions/core'
+import {promises as fs} from 'fs'
+
+export async function run(): Promise<void> {
+  const pathsOutputFile = core.getState('paths-output-file')
+
+  if (pathsOutputFile) {
+    await fs.unlink(pathsOutputFile)
+    core.info(`deleted paths-output-file: ${pathsOutputFile}`)
+  }
+}
+
+/* istanbul ignore if */
+if (!process.env.TESTING) {
+  // eslint-disable-next-line github/no-then
+  run().catch(e => {
+    core.setFailed(e.message || e)
+  })
+}
