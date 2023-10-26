f776bd936e84ee6e68e303fa990c3144606dc1d4
Andrew Haller <stairwaytowonderland@gmail.com>
2023-10-26 12:54:30 -0500

    Revert "Testing updates."
    This reverts commit c1f9115121055ded19410a5ba0444c1accbe43e1.

--- docker/bin/install-user.sh
+++ docker/bin/install-user.sh
@@ -29,12 +29,4 @@
     [ "$version" != "$latest_version" ] && "${tfenv_bin_path}/tfenv" install $latest_version
 
 # RUN \
-    cat > $HOME/.profile <<EOF
-
-# [ "\$UNAME" = "$(whoami)" ] || su -l \$UNAME
-[ "\$(pwd)" = "\$HOME" ] || cd ~
-
-EOF
-
-# RUN \
     chown -R "ubuntu:ubuntu" $HOMELOCAL
